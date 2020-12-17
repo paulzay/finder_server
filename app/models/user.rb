8fcc8a6341ea508994ff4cd259ea14bfb48b7d23
paulzay <paulotieno2@gmail.com>
2020-12-17 17:24:53 +0300

    add validation for email and comments about bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,26 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-    has_secure_password
-    validates_presence_of :username
-    validates :username, uniqueness: true, length: { minimum: 4 }
+  has_secure_password
+  validates_presence_of :username
+  validates_presence_of :email
+  validates :password_confirmation, presence: true
+  validates :username, uniqueness: true, length: { minimum: 4 }
+
+  # validates :password,  confirmation: true
+  # validates :password, presence: true, on: :create
+  # def password=(plaintext)
+      # encrypted= BCrypt::Password.create(plaintext)
+      # byebug
+      # self.password_digest=encrypted
+
+      # self.create(username:username)
+  # end
+
+  # def authenticate(plaintext)
+      # pw=BCrypt::Password.new(self.password_digest)
+      # byebug
+      # Eater.find_by(self.password_diget)
+  #     pw == plaintext
+  # end
 end
