ef7509ca177c2436d278854888d666b516ecba09
paulzay <paulotieno2@gmail.com>
2020-12-17 12:19:43 +0300

    write test for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
     has_secure_password
-    validates :username, presence: true, uniqueness: true, length: { minimum: 4 }
+    validates_presence_of :username
+    validates :username, uniqueness: true, length: { minimum: 4 }
 end
