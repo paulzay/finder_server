9ae74078697a0f646776d5cb5b8009bd8f455990
paulzay <paulotieno2@gmail.com>
2020-12-18 13:43:26 +0300

    generate favourites model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_18_081506) do
+ActiveRecord::Schema.define(version: 2020_12_18_103930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 2020_12_18_081506) do
     t.index ["user_id"], name: "index_cars_on_user_id"
   end
 
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "car_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["car_id"], name: "index_favorites_on_car_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
@@ -37,4 +46,6 @@ ActiveRecord::Schema.define(version: 2020_12_18_081506) do
   end
 
   add_foreign_key "cars", "users"
+  add_foreign_key "favorites", "cars"
+  add_foreign_key "favorites", "users"
 end
