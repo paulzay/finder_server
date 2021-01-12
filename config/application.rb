1d59a7ce049e56da4ec08f68c1767c40d754c0e5
paulzay <paulotieno2@gmail.com>
2021-01-12 12:15:05 +0300

    stuff

--- config/application.rb
+++ config/application.rb
@@ -33,10 +33,15 @@ module Finder
     config.api_only = true
     config.middleware.use ActionDispatch::Cookies    
     config.middleware.use ActionDispatch::Session::CookieStore
+
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource(
+          '*',
+          headers: :any,
+          methods: [:get, :patch, :put, :delete, :post, :options]
+          )
       end
     end
   end
