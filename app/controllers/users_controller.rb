d10b089171264ac9942ba9ffe5a9f21801c168cf
paulzay <paulotieno2@gmail.com>
2021-01-22 10:35:08 +0300

    move login to sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,18 +45,6 @@ class UsersController < ApplicationController
     end
   end
 
-  def login
-    @user = User.find_by(username: params[:username])
-
-    if @user&.authenticate(params[:password])
-      payload = { user_id: @user.id }
-      token = encode_token(payload)
-      render json: { user: @user, jwt: token }
-    else
-      render json: { error: 'Invalid username or password' }
-    end
-  end
-
   private
 
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :users, only: %i[create show index]
   resources :cars
   resources :favorites
-  post "/login", to: "users#login"
+  post "/login", to: "sessions#login"
   post '/logout', to: 'sessions#destroy'
 end
